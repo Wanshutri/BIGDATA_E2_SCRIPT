32f894d5a8f7a46353ae5949928644906d0b63c8
Wanshutri <valenzuelavivancofelipe@gmail.com>
2025-05-17 16:18:22 -0400

    testaso

--- comandos.sh
+++ comandos.sh
@@ -4,14 +4,14 @@ export BUCKET_NAME=mi-bucket-de-dataprep
 gsutil mb -l us-central1 gs://$BUCKET_NAME
 
 # 2. Subir el código a Cloud Run
-gcloud run deploy extraerParquet \
+gcloud run deploy extraer-parquet \
   --source container/. \
   --set-env-vars BUCKET_NAME=$BUCKET_NAME \
   --region us-central1 \
   --allow-unauthenticated
 
 # 3. Obtener la URL del servicio desplegado
-SERVICE_URL=$(gcloud run services describe extraerParquet --region us-central1 --format='value(status.url)')
+SERVICE_URL=$(gcloud run services describe extraer-parquet --region us-central1 --format='value(status.url)')
 
 echo "URL del servicio: $SERVICE_URL"
 
